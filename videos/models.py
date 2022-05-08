07cd59a563cd63ff831d0af02a68abd8b4cc19ce
ammetre <aritramitra2002@kgpian.iitkgp.ac.in>
2022-05-08 16:26:20 +0530

    Bugfix #1;

--- videos/models.py
+++ videos/models.py
@@ -4,7 +4,7 @@ from embed_video.fields import EmbedVideoField
 class Video(models.Model):
     title = models.CharField(max_length=100)
     added = models.DateTimeField(auto_now_add=True)
-    url = EmbedVideoField() 
+    url = EmbedVideoField(default="www.youtube.com/watch?v=qDwdMDQ8oX4") 
 
     def __str__(self):
         return str(self.title)
