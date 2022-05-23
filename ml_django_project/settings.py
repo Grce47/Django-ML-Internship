1ed69912785866fc21d7c5d86ffb3cbacf5c6e83
Ammetre <aritramitra2002@kgpian.iitkgp.ac.in>
2022-05-23 14:30:07 +0000

    Last Commit (Probably, from the production site this one is);

--- ml_django_project/settings.py
+++ ml_django_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import json
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$!902(jj0v60h*=io5i@g^#&xq%f#$-acxn%g=%5o**p^z-x*r'
+with open('/etc/config.json') as config_file:
+    config = json.load(config_file)
+
+SECRET_KEY = config['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['172.105.54.200']
 
 
 # Application definition
